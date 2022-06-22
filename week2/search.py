648d47788d727f0ae48304ab68b1dfae313afa22
ravi-vsc <ravikmr.tm@gmail.com>
2022-06-22 15:56:39 +0000

    l2

--- week2/search.py
+++ week2/search.py
@@ -62,10 +62,12 @@ def autocomplete():
         prefix = request.args.get("prefix")
         print(f"Prefix: {prefix}")
         if prefix is not None:
-            type = request.args.get("type", "queries") # If type == queries, this is an autocomplete request, else if products, it's an instant search request.
+            type_ = request.args.get("type", "queries") # If type == queries, this is an autocomplete request, else if products, it's an instant search request.
             ##### W2, L3, S1
             search_response = None
+
             print("TODO: implement autocomplete AND instant search")
+
             query_obj = {
                 "suggest": {
                     "autocomplete": {
@@ -81,6 +83,7 @@ def autocomplete():
             search_response = opensearch.search(
                 body=query_obj,
                 index="bbuy_queries" if type_ == "queries" else "bbuy_products")
+
             if (search_response and search_response['suggest']['autocomplete'] and search_response['suggest']['autocomplete'][0]['length'] > 0): # just a query response
                 results = search_response['suggest']['autocomplete'][0]['options']
     print(f"Results: {results}")
@@ -123,6 +126,7 @@ def query():
         ##### W2, L1, S2
 
         ##### W2, L2, S2
+        qu.add_spelling_suggestions(query_obj, user_query)
         print("Plain ol q: %s" % query_obj)
     elif request.method == 'GET':  # Handle the case where there is no query or just loading the page
         user_query = request.args.get("query", "*")
@@ -138,6 +142,7 @@ def query():
         #### W2, L1, S2
 
         ##### W2, L2, S2
+        qu.add_spelling_suggestions(query_obj, user_query)
 
     else:
         query_obj = qu.create_query("*", "", [], sort, sortDir, size=100)
@@ -153,4 +158,3 @@ def query():
                                sort=sort, sortDir=sortDir, explain=explain, autocompleteSelect=autocompleteSelect)
     else:
         redirect(url_for("index"))
-
